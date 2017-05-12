511875564b331f206700b0ecd6c5e696c81ec8a8
Deivid <deividscriollo@gmail.com>
2017-05-12 17:41:41 -0500

    procesosos

--- client/app/scripts/group/controller.js
+++ client/app/scripts/group/controller.js
@@ -4,6 +4,83 @@ angular.module('Group')
 .controller('group', function ($scope) {
 
   $scope.controller_loaded = 'Group loaded!';
+  // Proceso suma
+
+  $scope.suma = function(n1, n2){
+  	return n1+n2;
+  }
+
+  // proceso multiplicacion
+  $scope.multiplicacion = function(n1, n2){
+  	return n1 * n2;
+  }
+
+  $scope.validacion = function(datos){
+  	if (datos.length) {
+  		return true;
+  	}
+  	return false;
+  };
+
+
+  $scope.datos = function(datos){
+	if($scope.validacion(datos)){
+		if ($scope.proceso_estocolmo(datos)) {
+			console.log('test en camino', datos);
+		// 	if ($scope.proceso(datos)) {
+		// 		console.log('test en camino', datos);
+		// 	}
+			return true;
+		}
+		if (!$scope.proceso_estocolmo(datos)) {
+			return false;
+		}		
+	}
+
+	if(!$scope.validacion(datos)){
+		return false;
+	}
+  	
+  }
+  	$scope.acu = [];
+
+  	$scope.proceso_estocolmo = function(datos){
+  		var x = 0;
+	  	while (datos.length > x) {
+	  		var valor = datos[x];
+	  		if (!$scope.estocolmo(valor[0])) {
+	  			return false;
+	  		};	  		
+	  		x++;	
+	  	}
+		return true;
+  	}
+
+  	$scope.estocolmo = function(valor){
+  		if (valor >=1000 && valor <= 1999) {
+  			return true;
+  		}
+  		return false;
+  	}
+
+  // 	$scope.proceso_londres = function(datos){
+  // 		var x = 0;
+	 //  	while (datos.length > x) {
+	 //  		var valor = datos[x];
+	 //  		if (!$scope.londres(valor[1])) {
+	 //  			return false;
+	 //  		};	  		
+	 //  		x++;	
+	 //  	}
+		// return true;
+  // 	}
+
+  // 	$scope.londres = function(valor){
+  // 		if (valor >=1000 && valor <= 1999) {
+  // 			return true;
+  // 		}
+  // 		return false;
+  // 	}
 
 })
 .config(function ($routeProvider) {
@@ -13,3 +90,16 @@ angular.module('Group')
     controller: 'group'
   });
 });
+
+
+
+// for (var i = 0; i < datos.length; i++) {
+// 		var contador = 0;
+// 		// expression
+// 		if (valor[0] == datos[i][0]) {
+// 			$scope.acu = {valor[0], 0};
+// 		}
+// 		if (valor[0] == datos[i][1]) {
+// 			$scope.acu = ;
+// 		}
+// 	}
--- client/app/scripts/group/specs/controller_spec.js
+++ client/app/scripts/group/specs/controller_spec.js
@@ -16,6 +16,84 @@ describe('Controller: select group', function () {
     it('should set "controller_loaded" variable in scope', function () {
       expect(scope.controller_loaded).toContain('loaded');
     });
+    // -----------------------------add my code-----------------------------
+    // --------------------------nueva generacion --------------------------
+    it('should', function () {
+      var x = scope.suma(6, 1);
+      expect(x).toBe(7);
+
+      x = scope.suma(6, 3);
+      expect(x).toBe(9);
+
+       x = scope.suma(null, 3);
+      expect(x).toBe(3);
+
+
+       x = scope.suma(9, null);
+      expect(x).toBe(9);
+
+       x = scope.suma(null, null);
+      expect(x).toBe(0);
+
+      // vprueba multiplicacion
+          // prueba 1
+      var y = scope.multiplicacion(2, 2);
+          expect(y).toBe(4);
+          // prueba 2
+          y = scope.multiplicacion(2, 4);
+          expect(y).toBe(8);
+    });
+  });
+
+  describe('On instance', function () {
+    // proceso validacion
+    iit('should be equal to fffgfg', function () {
+      var datos = [
+        ['1009','2011'],
+        ['1097','2011']
+      ];
+      // prueba existencia
+      var res = scope.validacion(datos);
+          expect(res).toBe(true);
+
+          res = scope.validacion([]);
+          expect(res).toBe(false);
+          // prueba comparacion dato1 1000 - 1999
+          res = scope.datos([]);
+          expect(res).toBe(false);
+
+          res = scope.datos(datos);
+          expect(res).toBe(true);
+
+          // funcionario
+          res = scope.proceso_estocolmo(datos);
+          expect(res).toBe(true);
+
+          res = scope.proceso_estocolmo([]);
+          expect(res).toBe(false);
+
+          // res = scope.proceso('');
+          // expect(res).toBe(false);
+
+          // res = scope.estocolmo('1000');
+          // expect(res).toBe(true);
+
+          // res = scope.estocolmo('200');
+          // expect(res).toBe(false);
+
+          // res = scope.proceso1(datos);
+          // expect(res).toBe(true);
+          // res = scope.proceso1(datos);
+          // expect(res).toBe(false);
+
+          // res = scope.londres('2990');
+          // expect(res).toBe(true);
+
+          // res = scope.londres('20001');
+          // expect(res).toBe(false);
+
+
+    });
   });
 
   describe('when going to /group', function () {
